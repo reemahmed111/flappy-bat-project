1a8e9a95b90ba1c85319101c84c3fe64baeb8d2c
reemahmed111 <rey.blm@icloud.com>
2025-12-08 00:58:05 +0300

    loaded bat image

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
+//game
 let game;
 let context;
+let gameWidth = 400;
+let gameHeight = 600;
+
+//bat
+let batWidth = 50;
+let batHeight = 50;
+let batX = gameWidth/9;
+let batY = gameHeight/2.25;
+let batImg;
+
+
+let bat = {
+    x: batX,
+    y: batY,
+    width: batWidth,
+    height: batHeight
+}
+
 
 window.onload = function() {
     game = this.document.getElementById("game");
     context = game.getContext("2d");
-}
 
+    context.fillStyle = "green";
+    console.log("canvas loaded?, game, context")
+    context.fillRect(bat.x, bat.y, bat.width, bat.height);
+
+    //loading bat image
+    batImg = new Image();
+    batImg.src = "./bat.png"
+    //drawing the bat
+    batImg.onload = function(){
+    context.drawImage(batImg, bat.x, bat.y, bat.width, bat.height);
+    };
+}
