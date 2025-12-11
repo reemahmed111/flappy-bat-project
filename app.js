f6cb86841c727fe7a64ad4753c60d0c3d2a2a64e
reemahmed111 <rey.blm@icloud.com>
2025-12-11 20:05:28 +0300

    added an eventlistener to make the bat move

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ let gameHeight = 600;
 //bat
 let batWidth = 65;
 let batHeight = 65;
-let batX = gameWidth/9;
+let batX = gameWidth/12;
 let batY = gameHeight/2.25;
 let batImg;
 
 //pipes
 let pipeArray = [];
-let pipeWidth = 60;
+let pipeWidth = 40;
 let pipeHeight = 500;
 let pipeX = gameWidth;
 let pipeY = 0;
@@ -21,6 +21,11 @@ let pipeY = 0;
 let topPipeImg;
 let bottomPipeImg;
 
+
+//game physics
+let velocityX = -1.75;
+let velocityY = 0;
+let gravity = 0.5;
 
 
 let bat = {
@@ -46,17 +51,83 @@ window.onload = function() {
     batImg.onload = function(){
     context.drawImage(batImg, bat.x, bat.y, bat.width, bat.height);
     };
+    
+    //loading pipes images
+    topPipeImg = new Image();
+    topPipeImg.src = "./topPipe.png"
 
-    //requestAnimationFrame(update);
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "./bottomPipe.png"
+
+
+        requestAnimationFrame(update);
+     setInterval(placePipes, 1800);
+
+     document.addEventListener("keydown", moveBat);
 }
 
- /*function update() {
+
+ function update() {
     requestAnimationFrame(update);
-    context.clearRect(0, 0, game.width, game.height)
 
-    context.drawImg(batImg, bat.x, bat.y, bat.width, bat.height);
+    velocityY += gravity;    
+    bat.y += velocityY;
+
+     context.clearRect(0, 0, gameWidth, gameHeight);
+    context.drawImage(batImg, bat.x, bat.y, bat.width, bat.height);
+
+    //pipes loop
+    for (let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height)
+        
+    }};
+
+     
+
+
+//pipes function
+function placePipes() { 
+    
+    let gap = 190;  
+      
+
+    let minTopY = -pipeHeight + 30;
+    let maxTopY = - 20; 
+
+    let randomY = Math.random() * (maxTopY - minTopY) + minTopY;
+
+
+    let topPipe = {
+    img: topPipeImg,
+    x: pipeX,
+    y: randomY,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false
+    };
+
+
+    let bottomPipe = {
+        img: bottomPipeImg,
+        x: gameWidth,
+        y:  randomY + pipeHeight + gap,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    };
+
+    //adding it to array
+    pipeArray.push(topPipe);
+    pipeArray.push(bottomPipe);
+
 
 }
-    */
+    
 
-
+function moveBat(e) {
+    if (e.code == "space" || e.code == "ArrowUp" || e.code == "KeyX") {
+        velocityY = -6;
+    }
+}
